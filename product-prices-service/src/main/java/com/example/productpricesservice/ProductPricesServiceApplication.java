d5829d9717ef433e349d065372e624f6114ac8de
José Miguel Chavez <jose.chavez.g@usach.cl>
2022-07-06 14:29:00 -0400

    a

---
+++ product-prices-service/src/main/java/com/example/productpricesservice/ProductPricesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.productpricesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPricesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPricesServiceApplication.class, args);
+	}
+
+}
